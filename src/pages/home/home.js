a9e20c02ed7670aaae8e764f11f363765529e07c
gloriaumutoni <gloriaumutoni54@gmail.com>
2023-05-30 23:18:07 +0300

    deployment of transport tech soln

---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './home.css'
+import Image from '../image/ElecMobility.jpg'
+
+const Home =()=>{
+    return(
+        <div>
+<img src={Image}/>
+        </div>
+    )
+}
+export default Home
